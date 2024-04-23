f643b7b2893342a0f148cb889dc9cd89275b983b
Eda <160626133+edakarakocc@users.noreply.github.com>
2024-04-23 16:16:33 +0300

    Update uyekontrol.py

--- uyekontrol.py
+++ uyekontrol.py
@@ -1,56 +1,35 @@
 def personel_kontrol():
     global kontrol
-    x=False
-    ad=input("Adinizi girin:")
-    soyad=input("Sayadinizi girin:")
-    sifre=input("Sifrenizi girin:")
-    with open("personeller.txt","r") as dosya:
-        liste=[]
+    x = False
+    ad = input("Adınızı girin:")
+    soyad = input("Soyadınızı girin:")
+    sifre = input("Şifrenizi girin:")
+    
+    with open("personeller.txt", "r") as dosya:
         for satir in dosya:
-            icerik=satir.strip().split(",")
-            liste.append(icerik)
-        personelmi=[]
-        personelmi.append(ad)
-        personelmi.append(soyad)
-        personelmi.append(sifre)
-    for i in range(len(liste)):
-        if liste[i][2]==personelmi[2] and liste[i][1]==personelmi[1]:
-            if liste[i][0]==personelmi[0]:
-                print("\nDogrulama basarili")
-                x=True
-            else:
-                x=False
-    if x:
-        kontrol=True
-    else:
-        kontrol=False
-    personelmi=[]
+            icerik = satir.strip().split(",")
+            if len(icerik) == 3: 
+                if icerik[0] == ad and icerik[1] == soyad and icerik[2] == sifre:
+                    print("\nDoğrulama başarılı")
+                    x = True
+                    break             
+    kontrol = x
     return kontrol
+
 def ogrenci_kontrol():
     global dogrumu
-    x=False
-    ad=input("Adinizi girin:")
-    soyad=input("Sayadinizi girin:")
-    sifre=input("Sifrenizi girin:")
-    with open("ogrenciler.txt","r") as file:
-        liste=[]
-        for satir in file:
-            icerik=satir.strip().split(",")
-            liste.append(icerik)
-    ogrencimi=[]
-    ogrencimi.append(ad)
-    ogrencimi.append(soyad)
-    ogrencimi.append(sifre)
-    for i in range(len(liste)):
-        if liste[i][2]==ogrencimi[2] and liste[i][1]==ogrencimi[1]:
-            if liste[i][0]==ogrencimi[0]:
-                print("\nDogrulama basarili")
-                x=True
-            else:
-                x=False
-    if x:
-        dogrumu=True
-    else:
-        dogrumu=False 
-    ogrencimi=[]
+    x = False
+    ad = input("Adınızı girin:")
+    soyad = input("Soyadınızı girin:")
+    sifre = input("Şifrenizi girin:")
+    
+    with open("ogrenciler.txt", "r") as dosya:
+        for satir in dosya:
+            icerik = satir.strip().split(",")
+            if len(icerik) == 3:  
+                if icerik[0] == ad and icerik[1] == soyad and icerik[2] == sifre:
+                    print("\nDoğrulama başarılı")
+                    x = True
+                    break              
+    dogrumu = x
     return dogrumu
