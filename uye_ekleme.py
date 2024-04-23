48b69f67c68d4a0683d9acd6dfbb2334b9178c6b
Eda <160626133+edakarakocc@users.noreply.github.com>
2024-04-23 16:16:57 +0300

    Update uye_ekleme.py

--- uye_ekleme.py
+++ uye_ekleme.py
@@ -1,26 +1,26 @@
 def personel_ekle():
     try:
           global personel_ad
-          personel_ad=input("Adinizi girin:")
+          personel_ad=input("Adınızı girin:")
           global personel_soyad
-          personel_soyad=input("Soyadinizi girin:")
+          personel_soyad=input("Soyadınızı girin:")
     except:
-        print("!!!Hatali tipte giris yaptiniz girdiğiniz bilgileri tekrar kontrol edin.!!!")
+        print("!!!Hatali tipte giriş yaptınız girdiğiniz bilgileri tekrar kontrol edin.!!!")
     global personel_sifre
     personel_sifre=(len(personel_ad)+len(personel_soyad))*3
-    print("Sifreniz:",personel_sifre,"kimseyle paylaşmayiniz")
+    print("Şifreniz:",personel_sifre,"kimseyle paylaşmayınız")
     with open("personeller.txt","a") as f:
         f.write(f"{personel_ad},{personel_soyad},{str(personel_sifre)}\n")
 def ogrenci_ekle():
     try:
         global ogrenciAd
-        ogrenciAd=input("Adinizi girin:")   #kullanicidan bilgiler isteniyor
+        ogrenciAd=input("Adınızı girin:")   #kullanicidan bilgiler isteniyor
         global ogrenciSoyad
-        ogrenciSoyad=input("Soyadinizi girin:")
+        ogrenciSoyad=input("Soyadınızı girin:")
     except:
-        print("!!!Hatali tipte giris yaptiniz girdiğiniz bilgileri tekrar kontrol edin.!!!")
+        print("!!!Hatali tipte giriş yaptınız girdiğiniz bilgileri tekrar kontrol edin.!!!")
     global ogrenci_sifre
     ogrenci_sifre=(len(ogrenciAd)+len(ogrenciSoyad))*11
-    print("Sifreniz:",ogrenci_sifre,"kimseyle paylaşmayiniz")
+    print("Şifreniz:",ogrenci_sifre,"kimseyle paylaşmayınız")
     with open("ogrenciler.txt","a") as file:
         file.write(f"{ogrenciAd},{ogrenciSoyad},{str(ogrenci_sifre)}\n")
